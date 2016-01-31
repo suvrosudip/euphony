915c7f418209de0b1fa42b6ae20cc064eae86db6
suvrosudip <suvrossudip@gmail.com>
2016-01-31 01:00:34 -0500

    Euphony App v1.1.0.0
    Changes:
    1. New media player interface with scrubber scrolling, play/pause, stop
    button logic.

--- js/driver.js
+++ js/driver.js
@@ -6,19 +6,74 @@ function updateSource(song){
 	//alert(source.src);
 	player.load();
 	player.play();
-	//document.getElementById('source_change').data-value=document.getElementById(song).getAttribute('data-value');
-	
-	
+	playButton.className = "";
+	playButton.className = "pause";
 		
 }
 
-function pause() {
-	player.pause();
+var activeSong;
+//Does a switch of the play/pause with one button.
+function playPause(id){
+	var playButton = document.getElementById('playButton'); // play button
+    //Sets the active song since one of the functions could be play.
+    activeSong = document.getElementById('player');
+    //Checks to see if the song is paused, if it is, play it from where it left off otherwise pause it.
+    if (activeSong.paused){
+         activeSong.play();
+		playButton.className = "";
+			playButton.className = "pause";
+    }else{
+         activeSong.pause();
+		 playButton.className = "";
+		playButton.className = "play";
+    }
 }
 
-function play() {
-	player.play();
+//Updates the current time function so it reflects where the user is in the song.
+//This function is called whenever the time is updated.  This keeps the visual in sync with the actual time.
+function updateTime(){
+	activeSong = document.getElementById('player');
+    var currentSeconds = (Math.floor(activeSong.currentTime % 60) < 10 ? '0' : '') + Math.floor(activeSong.currentTime % 60);
+    var currentMinutes = Math.floor(activeSong.currentTime / 60);
+    //Sets the current song location compared to the song duration.
+    document.getElementById('songTime').innerHTML = currentMinutes + ":" + currentSeconds + ' / ' + Math.floor(activeSong.duration / 60) + ":" + (Math.floor(activeSong.duration % 60) < 10 ? '0' : '') + Math.floor(activeSong.duration % 60);
+
+    //Fills out the slider with the appropriate position.
+    var percentageOfSong = (activeSong.currentTime/activeSong.duration);
+	var percentageOfSlider = document.getElementById('songSlider').offsetWidth * percentageOfSong;
+    
+    //Updates the track progress div.
+    document.getElementById('trackProgress').style.width = Math.round(percentageOfSlider) + "px";
 }
+
+
+//Sets the location of the song based off of the percentage of the slider clicked.
+function setLocation(percentage){
+    activeSong.currentTime = activeSong.duration * percentage;
+}
+/*
+Gets the percentage of the click on the slider to set the song position accordingly.
+Source for Object event and offset: http://website-engineering.blogspot.com/2011/04/get-x-y-coordinates-relative-to-div-on.html
+*/
+function setSongPosition(obj,e){
+    //Gets the offset from the left so it gets the exact location.
+    var songSliderWidth = obj.offsetWidth;
+    var evtobj=window.event? event : e;
+    clickLocation =  evtobj.layerX - obj.offsetLeft;
+    
+    var percentage = (clickLocation/songSliderWidth);
+    //Sets the song location with the percentage.
+    setLocation(percentage);
+}
+
+
+
+//Stop song by setting the current time to 0 and pausing the song.
+function stopSong(){
+    activeSong.currentTime = 0;
+    activeSong.pause();
+}
+
 
 
 
